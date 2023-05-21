2634c7d896e164cdb29d8c673ea7109debf773f4
Kaleb Lamar <lamarkaleb@gmail.com>
2023-05-20 19:08:51 -0500

    added set timeout so audio matched better with opening video

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 console.log(`hello`);
 
 const themeSong = document.querySelector(`#themeSong`);
-window.onload = themeSong.play();
+
+setTimeout(() => {
+  window.onload = themeSong.play();
+}, 1200);
+
+const coin = new Audio(`/FroggerJS/Audio/sound-frogger-coin-in.wav`);
+
+const btnSound = document.querySelector(`button`);
+btnSound.addEventListener(`click`, function () {
+  coin.play();
+});
